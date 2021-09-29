c8dcb33e3791e3840acdfee0dbbf400cb9b58165
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-09-29 17:20:39 -0500

    folders

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 
-
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
